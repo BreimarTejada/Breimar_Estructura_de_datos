7b5f380f22a515cc30fcfa702b2dd375b4288b0c
BreimarTejada <btejada@ucundinamarca.edu.co>
2025-04-06 22:06:45 -0500

    Update Main.java

---
+++ R2-A1-S8 Trivia de Palabras Relacionadas/trivia/src/Cliente.java
@@ -0,0 +1,18 @@
+//clase de los datos de los clientes
+
+public class cliente {
+String Cedula;
+String Nombre;
+
+Public cliente(String Cedula, String Nombre){
+this.Cedula = Cedula;
+this.Nombre = Nombre;
+}
+
+@Override
+Public String toString{
+    return "Cedula: " + Cedula + ", Nombre: " + Nombre;
+}
+
+}
+
---
+++ R2-A1-S8 Trivia de Palabras Relacionadas/trivia/src/Nodo.java
@@ -0,0 +1,10 @@
+//nodo de la lista enlazada simple
+
+Public class nodo{
+    Cliente cliente;
+    Nodo siguiente;
+Public Nodo(Cliente cliente){
+    this.cliente = cliente;
+    this.siguiente = null;
+}
+}
